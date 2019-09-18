44ac9972a5a8110ce29d762a25dc3d69a82fc0ea
Anastasia Legert <legertlab@gmail.com>
2019-09-18 12:33:25 +0200

    Task fetch all done

--- script.js
+++ script.js
@@ -18,46 +18,33 @@ fetch('https://api.github.com/users/' + name)
         let name = json.name;
         let bio = json.bio;
         let profile =json.html_url;
-        // console.log(json.name);
-        // let createAvatar = () => {
-        //     let newAvatar = document.createElement('img');
-        //     newAvatar.src = avatar;
-        //     let addString = document.createElement('br');
-        //     document.body.appendChild(newAvatar);
-        //     document.body.appendChild(addString);
-        // }
-        // let createName = () => {
-        //     let newName = document.createElement('h2');
-        //     newName.innerHTML = name;
-        //     document.body.appendChild(newName);
-        // }
 
-        // let createBio = () => {
-        //     let newBio = document.createElement('p');
-        //     newBio.innerHTML = bio;
-        //     document.body.appendChild(newBio);
-        // }
+        let createAvatar = () => {
+            let newAvatar = document.createElement('img');
+            newAvatar.src = avatar;
+            let addString = document.createElement('br');
+            document.body.appendChild(newAvatar);
+            document.body.appendChild(addString);
+        }
 
-        // let createProfile = () => {
-        //     let newProfile = document.createElement('a');
-        //     newProfile.href = profile;
-        //     document.body.appendChild(newProfile);
-        //     document.body.newProfile.appendChild(createName());
-        //     // document.body.newProfile.appendChild(createName());
-        // }
+        let createBio = () => {
+            let newBio = document.createElement('p');
+            newBio.innerHTML = bio;
+            document.body.appendChild(newBio);
+        }
+
         let createProfile = () => {
             let elementForLink = document.createElement('a');
             let elementForHeader = document.createElement('h2');
-            
+            elementForHeader.innerText = name;
+            elementForLink.href = profile;
             document.body.appendChild(elementForLink);
-            document.body.elementForLink.appendChild(elementForHeader);
-            elementForHeader.innerText('hello1');
+            elementForLink.appendChild(elementForHeader);
         }
-        createProfile();
-        // createBio();
-        // createAvatar();
-        // createName();
 
+        createProfile();
+        createBio();
+        createAvatar();
     })
 
-    .catch(err => console.log(err));
+    .catch(err => alert(err + ' Профиль не найден'));
